9019b00a06f63ddafa597e52642b15def6f6e0f3
Junio Henrique <juunio.henriquee@gmail.com>
2022-02-02 11:52:23 -0300

    fix broken testing error aIOf

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -4,20 +4,11 @@ describe('The Home Page', () => {
       // Teste realizado na resolução 1280x720
       cy.viewport(1280, 720)
     })
-
-
-  it('Mapa visivel somente após o click', () => {
-    cy.visit('/') 
-    cy.clickLink('Pra Você')
-    cy.clickLink('Onde usar meu cartão VR?')
-    cy.get('.vr-container__mapa').should('be.visible')
+    it('Mapa visivel somente após o click', () => {
+      cy.visit('/') 
+      cy.clickLink('Pra Você')
+      cy.clickLink('Onde usar meu cartão VR?')
+      cy.get('.vr-container__mapa').should('be.visible')
+    })
   })
-
-
-  it('Mapa invisível sem click no link', () => {
-    cy.visit('/') 
-    cy.clickLink('Pra Você')
-    cy.get('.vr-container__mapa').should('not.be.visible')
-  })
-})
 })
